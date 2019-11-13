3737bc38a46a9be59d28e2138855d30e7ea5eba6
rimmmming <jrim1307@gmail.com>
2019-11-14 00:50:13 +0900

    build

---
+++ build/precache-manifest.ad0c2207489ff618c5ae94b6a0b9fd18.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3aebd1f771c9d6be82f6fb405342c9f",
+    "url": "/auction_srp/index.html"
+  },
+  {
+    "revision": "bdff198a7c30b6f7b20e",
+    "url": "/auction_srp/static/js/2.a08c188f.chunk.js"
+  },
+  {
+    "revision": "1e1475b6c4d0e6dd1ca3",
+    "url": "/auction_srp/static/js/main.e9244461.chunk.js"
+  },
+  {
+    "revision": "d18e53f21c003f663180",
+    "url": "/auction_srp/static/js/runtime~main.3e5b2e8e.js"
+  }
+]);
